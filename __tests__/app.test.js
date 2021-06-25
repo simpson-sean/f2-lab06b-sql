3b0836c07ed4f54fbc8f600f85b9d794f7e3039b
Sean <simpson.sean@outlook.com>
2021-06-25 14:30:40 -0700

    WORKDONE: Consolidate GET testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,93 +1,37 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const { execSync } = require('child_process');
+// const { execSync } = require('child_process');
 
-const fakeRequest = require('supertest');
-const app = require('../lib/app');
-const client = require('../lib/client');
+// const fakeRequest = require('supertest');
+// const app = require('../lib/app');
+// const client = require('../lib/client');
 
-describe('app routes', () => {
-  describe('routes', () => {
-    let token;
+// describe('app routes', () => {
+//   describe('routes', () => {
+//     let token;
   
-    beforeAll(async done => {
-      execSync('npm run setup-db');
+//     beforeAll(async done => {
+//       execSync('npm run setup-db');
   
-      client.connect();
+//       client.connect();
       
-      const signInData = await fakeRequest(app)
-        .post('/auth/signup')
-        .send({
-          email: 'test@test.com',
-          password: '1234'
-        });
+//       const signInData = await fakeRequest(app)
+//         .post('/auth/signup')
+//         .send({
+//           email: 'test@test.com',
+//           password: '1234'
+//         });
       
 
-      token = signInData.body.token; // eslint-disable-line
+//       token = signInData.body.token; // eslint-disable-line
   
-      return done();
-    }, 10000);
+//       return done();
+//     });
   
-    afterAll(done => {
-      return client.end(done);
-    });
+//     afterAll(done => {
+//       return client.end(done);
+//     });
 
-    test('/GET returns trek_character', async() => {
-  
-        const expectation = [
-            {
-                "id": 3,
-                "name": "spock",
-                "species": "vulcan",
-                "faction": "starfleet",
-                "category": "science",
-                "rank": "commander",
-                "is_carbon_based": true
-            },
-            {
-                "id": 2,
-                "name": "data",
-                "species": "android",
-                "faction": "starfleet",
-                "category": "command",
-                "rank": "lieutenant_commander",
-                "is_carbon_based": false
-            },
-            {
-                "id": 1,
-                "name": "jean_luc_picard",
-                "species": "human",
-                "faction": "starfleet",
-                "category": "command",
-                "rank": "captain",
-                "is_carbon_based": true
-            },
-            {
-                "id": 5,
-                "name": "general_martok",
-                "species": "klingon",
-                "faction": "klingon_empire",
-                "category": "military",
-                "rank": "general",
-                "is_carbon_based": true
-            },
-            {
-                "id": 4,
-                "name": "weyoun",
-                "species": "vorta",
-                "faction": "dominion",
-                "category": "supervisor",
-                "rank": "supervisor",
-                "is_carbon_based": true
-            }
-        ];
-  
-        const data = await fakeRequest(app)
-          .get('/trek_character')
-          .expect('Content-Type', /json/)
-          .expect(200);
-  
-        expect(data.body).toEqual(expectation);
-      });
-  });
-});
+    
+//   });
+// });
